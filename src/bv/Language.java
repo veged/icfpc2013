1fbcfad93a5e6e24c01df20e6d78fb608f1d207b
Yuri Klimov <yuri@klimov.net>
2013-08-11 00:35:20 +0400

    - Генерация программ с ограниченным набором операторов.

--- src/bv/Language.java
+++ src/bv/Language.java
@@ -7,40 +7,48 @@ public class Language {
     public static final Var y = new Var(false, true, 'y');
     public static final Var z = new Var(false, true, 'z');
 
+    public static Expression op1 (Op1.OpName op, Expression e) {
+        return new Op1(op, e);
+    }
+
     public static Expression not (Expression e) {
-        return new Op1(Op1.OpName.not, e);
+        return op1(Op1.OpName.not, e);
     }
 
     public static Expression shl1 (Expression e) {
-        return new Op1(Op1.OpName.shl1, e);
+        return op1(Op1.OpName.shl1, e);
     }
 
     public static Expression shr1 (Expression e) {
-        return new Op1(Op1.OpName.shr1, e);
+        return op1(Op1.OpName.shr1, e);
     }
 
     public static Expression shr4 (Expression e) {
-        return new Op1(Op1.OpName.shr4, e);
+        return op1(Op1.OpName.shr4, e);
     }
 
     public static Expression shr16 (Expression e) {
-        return new Op1(Op1.OpName.shr16, e);
+        return op1(Op1.OpName.shr16, e);
     }
 
+    public static Expression op2 (Op2.OpName op, Expression e1, Expression e2) {
+        return new Op2(op, e1, e2);
+    }
+   
     public static Expression and (Expression e1, Expression e2) {
-        return new Op2(Op2.OpName.and, e1, e2);
+        return op2(Op2.OpName.and, e1, e2);
     }
 
     public static Expression or (Expression e1, Expression e2) {
-        return new Op2(Op2.OpName.or, e1, e2);
+        return op2(Op2.OpName.or, e1, e2);
     }
 
     public static Expression xor (Expression e1, Expression e2) {
-        return new Op2(Op2.OpName.xor, e1, e2);
+        return op2(Op2.OpName.xor, e1, e2);
     }
 
     public static Expression plus (Expression e1, Expression e2) {
-        return new Op2(Op2.OpName.plus, e1, e2);
+        return op2(Op2.OpName.plus, e1, e2);
     }
 
     public static Expression if0 (Expression e0, Expression e1, Expression e2) {
