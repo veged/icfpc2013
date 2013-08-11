452162c7606b3a86ed3800b579651cf710c36af1
Sergey Berezhnoy <veged@yandex-team.ru>
2013-08-11 14:32:13 +0400

    .filter() and .any() stub for meta expressions

--- src/bv/Wildcard.java
+++ src/bv/Wildcard.java
@@ -14,11 +14,6 @@ public class Wildcard extends Expression {
     }
 
     @Override
-    public Expression any() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
     public String toString() {
         return "(*" + size + ")";
     }
